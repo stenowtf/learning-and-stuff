ebfec6023b15ca34df39a4de0ea2df71a49d59e6
Stefano Canducci <steno@fastmail.fm>
2017-01-03 12:32:08 +0100

    starting postgres

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -1,22 +1,3 @@
 defmodule Rumbl.Repo do
-  @moduledoc """
-  In memory repository.
-  """
-
-  def all(Rumbl.User) do
-    [%Rumbl.User{id: "1", name: "steno c", username: "ste", password: "qwe"},
-      %Rumbl.User{id: "2", name: "john b", username: "jo", password: "asd"},
-      %Rumbl.User{id: "3", name: "kain l", username: "ka", password: "zxc"}]
-  end
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
+  use Ecto.Repo, otp_app: :rumbl
 end
