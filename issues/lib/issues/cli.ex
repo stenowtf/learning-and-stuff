aa6bd01ff711d5058f493d3f2ea4484f76e5cabf
Stefano Canducci <steno@fastmail.fm>
2017-02-08 20:56:18 +0100

    table, tests, executable

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -1,4 +1,6 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
+
   @default_count 4
 
   @moduledoc """
@@ -6,7 +8,7 @@ defmodule Issues.CLI do
   the various functions that end up generating a
   table of the last _n_ issues in a GitHub project
   """
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
@@ -46,6 +48,7 @@ defmodule Issues.CLI do
     |> decode_response
     |> sort_into_ascending_order
     |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
--- issues/mix.exs
+++ issues/mix.exs
@@ -3,6 +3,7 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
+     escript: escript_config(),
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
@@ -32,5 +33,9 @@ defmodule Issues.Mixfile do
       {:httpoison, "~> 0.11.0"},
       {:poison, "~> 3.1"}
     ]
+  end
+
+  defp escript_config do
+    [main_module: Issues.CLI]
   end
 end
