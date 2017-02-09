9348c40918f9368599628337018fcf74a770dd1a
Stefano Canducci <steno@fastmail.fm>
2017-02-09 19:43:38 +0100

    test coverage and graph

--- issues/mix.exs
+++ issues/mix.exs
@@ -2,15 +2,24 @@ defmodule Issues.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :issues,
-     name: "Issues",
-     source_url: "https://github.com/stenowtf/learning-and-stuff/tree/master/issues",
-     escript: escript_config(),
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :issues,
+      name: "Issues",
+      source_url: "https://github.com/stenowtf/learning-and-stuff/tree/master/issues",
+      escript: escript_config(),
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+      ],
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -35,7 +44,8 @@ defmodule Issues.Mixfile do
       {:httpoison, "~> 0.11.0"},
       {:poison, "~> 3.1"},
       {:ex_doc, "~> 0.14.5"},
-      {:earmark, "~> 1.1", override: true}
+      {:earmark, "~> 1.1", override: true},
+      {:excoveralls, "~> 0.6.2", only: :test}
     ]
   end
 
