25327092b67b1ba4a86e0ebfe829ecd8dc360a48
Stefano Canducci <steno@fastmail.fm>
2017-02-11 09:26:22 +0100

    chapter 15, exercise 2

--- exercises/ch15-ex1-2.exs
+++ exercises/ch15-ex1-2.exs
@@ -1,3 +1,6 @@
+# 15.1
+# Run this code on your machine. See if you get comparable results.
+
 defmodule Chain do
   def counter(next_pid) do
     receive do
@@ -25,14 +28,43 @@ defmodule Chain do
   end
 end
 
-# elixir -r ch15-ex1-2.exs -e "Chain.run(10)"
+# $ elixir -r ch15-ex1-2.exs -e "Chain.run(10)"
 
 # {6963, "Result is 10"}
 # {8202, "Result is 100"}
 # {17786, "Result is 1000"}
 # {153255, "Result is 10000"}
 
-# elixir --erl "+P 1000000" -r ch15-ex1-2.exs -e "Chain.run(400_000)"
+# $ elixir --erl "+P 1000000" -r ch15-ex1-2.exs -e "Chain.run(400_000)"
 
 # {5550471, "Result is 400000"}
 # {15611937, "Result is 1000000"}
+
+# 15.2
+# Write a program that spawns two processes and then passes each a unique token (for example, “fred” and “betty”). Have them send the tokens back.
+# – Is the order in which the replies are received deterministic in theory? In practice?
+# – If either answer is no, how could you make it so?
+
+defmodule Ping do
+  def receiver do
+    receive do
+      {who, message} ->
+        send who, {:ok, message}
+    end
+  end
+
+  def sender do
+    receive do
+      {:ok, message} ->
+        IO.puts message
+        sender()
+    end
+  end
+end
+
+sender = spawn(Ping, :sender, [])
+first_receiver = spawn(Ping, :receiver, [])
+second_receiver = spawn(Ping, :receiver, [])
+
+send first_receiver, {sender, "ping"}
+send second_receiver, {sender, "pong"}
