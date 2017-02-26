66c19a630b63fbf2edc4c6650ab3305e6ea168cf
Stefano Canducci <steno@fastmail.fm>
2017-02-26 15:29:16 +0100

    1.15 add

--- fun-in-erlang/patterns.erl
+++ fun-in-erlang/patterns.erl
@@ -1,5 +1,5 @@
 -module(patterns).
--export([xOr_1/2, xOr_2/2, maxThree/3, howManyEqual/3]).
+-export([xOr_1/2, xOr_2/2, xOr_3/2, maxThree/3, howManyEqual/3]).
 
 xOr_1(true, false) ->
     true;
@@ -11,7 +11,12 @@ xOr_1(_, _) ->
 xOr_2(X, X) -> false;
 xOr_2(_, _) -> true.
 
-% xOr_3 ? xOr_4 ? xOr_5 ? :(
+xOr_3(true, X) ->
+    not(X);
+xOr_3(false, X) ->
+    X.
+
+% xOr_4 ? xOr_5 ? :(
 
 maxThree(A, B, C) ->
     max(max(A, B), C).
