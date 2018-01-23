55624381bef74a4cada1fea519b6779d227b35b8
Stefano Canducci <steno@fastmail.fm>
2018-01-23 16:27:07 +0100

    chapter 2

--- islands_engine/lib/island_engine/board.ex
+++ islands_engine/lib/island_engine/board.ex
@@ -22,11 +22,50 @@ defmodule IslandsEngine.Board do
     |> guess_response(board)
   end
 
-
-
   defp overlaps_existing_island?(board, new_key, new_island) do
     Enum.any?(board, fn {key, island} ->
       key != new_key and Island.overlaps?(island, new_island)
     end)
+  end
+
+  defp check_all_islands(board, coordinate) do
+    Enum.find_value(board, :miss, fn {key, island} ->
+      case Island.guess(island, coordinate) do
+        {:hit, island} -> {key, island}
+        :miss -> false
+      end
+    end)
+  end
+
+  defp guess_response({key, island}, board) do
+    board = %{board | key => island}
+    {:hit, forest_check(board, key), win_check(board), board}
+  end
+  defp guess_response(:miss, board) do
+    {:miss, :none, :no_win, board}
+  end
+
+  defp forest_check(board, key) do
+    case forested?(board, key) do
+      true -> key
+      false -> :none
+    end
+  end
+
+  defp forested?(board, key) do
+    board
+    |> Map.fetch!(key)
+    |> Island.forested?()
+  end
+
+  defp win_check(board) do
+    case all_forested?(board) do
+      true -> :win
+      false -> :no_win
+    end
+  end
+
+  defp all_forested?(board) do
+    Enum.all?(board, fn {_key, island} -> Island.forested?(island) end)
   end
 end
