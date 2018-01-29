4d332013cc4533085b8b6cb8411554c75ae499f4
Stefano Canducci <steno@fastmail.fm>
2018-01-29 12:45:56 +0100

    chapter 4 - part 2

--- islands_engine/lib/islands_engine/game.ex
+++ islands_engine/lib/islands_engine/game.ex
@@ -6,7 +6,11 @@ defmodule IslandsEngine.Game do
   @players [:player1, :player2]
 
   def start_link(name) when is_binary(name) do
-    GenServer.start_link(__MODULE__, name, [])
+    GenServer.start_link(__MODULE__, name, name: via_tuple(name))
+  end
+
+  def via_tuple(name) do
+    {:via, Registry, {Registry.Game, name}}
   end
 
   def init(name) do
@@ -25,6 +29,10 @@ defmodule IslandsEngine.Game do
 
   def set_islands(game, player) when player in @players do
     GenServer.call(game, {:set_islands, player})
+  end
+
+  def guess_coordinate(game, player, row, col) when player in @players do
+    GenServer.call(game, {:guess_coordinate, player, row, col})
   end
 
   def handle_call({:add_player, name}, _from, state_data) do
@@ -70,6 +78,26 @@ defmodule IslandsEngine.Game do
     end
   end
 
+  def handle_call({:guess_coordinate, player_key, row, col}, _from, state_data) do
+    opponent_key = opponent(player_key)
+    opponent_board = player_board(state_data, opponent_key)
+
+    with {:ok, rules} <- Rules.check(state_data.rules, {:guess_coordinate, player_key}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {hit_or_miss, forested_island, win_status, opponent_board} <-
+           Board.guess(opponent_board, coordinate),
+         {:ok, rules} <- Rules.check(rules, {:win_check, win_status}) do
+      state_data
+      |> update_board(opponent_key, opponent_board)
+      |> update_guesses(player_key, hit_or_miss, coordinate)
+      |> update_rules(rules)
+      |> reply_success({hit_or_miss, forested_island, win_status})
+    else
+      :error -> {:reply, :error, state_data}
+      {:error, :invalid_coordinate} -> {:reply, {:error, :invalid_coordinate}, state_data}
+    end
+  end
+
   defp update_player2_name(state_data, name) do
     put_in(state_data.player2.name, name)
   end
@@ -88,5 +116,19 @@ defmodule IslandsEngine.Game do
 
   defp player_board(state_data, player) do
     Map.get(state_data, player).board
+  end
+
+  defp opponent(:player1) do
+    :player2
+  end
+
+  defp opponent(:player2) do
+    :player1
+  end
+
+  defp update_guesses(state_data, player_key, hit_or_miss, coordinate) do
+    update_in(state_data[player_key].guesses, fn guesses ->
+      Guesses.add(guesses, hit_or_miss, coordinate)
+    end)
   end
 end
