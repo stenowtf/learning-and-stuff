56c12b3b21ab370b9800779b60d54688a7a0da3c
Stefano Canducci <steno@fastmail.fm>
2018-01-30 15:27:16 +0100

    chapter 6

--- islands/islands_interface/lib/islands_interface_web/router.ex
+++ islands/islands_interface/lib/islands_interface_web/router.ex
@@ -2,21 +2,23 @@ defmodule IslandsInterfaceWeb.Router do
   use IslandsInterfaceWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", IslandsInterfaceWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+    post("/test", PageController, :test)
   end
 
   # Other scopes may use custom stacks.
