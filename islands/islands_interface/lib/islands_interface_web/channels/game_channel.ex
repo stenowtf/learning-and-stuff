b2773ca50531c0748e19b4d0b21a4257bdc66c5b
Stefano Canducci <steno@fastmail.fm>
2018-01-31 17:21:48 +0100

    chapter 7 - part 2

--- islands/islands_interface/lib/islands_interface_web/channels/game_channel.ex
+++ islands/islands_interface/lib/islands_interface_web/channels/game_channel.ex
@@ -1,9 +1,18 @@
 defmodule IslandsInterfaceWeb.GameChannel do
   use IslandsInterfaceWeb, :channel
 
-  alias IslandsInterface.{Game, GameSupervisor}
+  alias IslandsEngine.{Game, GameSupervisor}
 
   def join("game:" <> _player, _payload, socket) do
     {:ok, socket}
+  end
+
+  def handle_in("new_game", _payload, socket) do
+    "game:" <> player = socket.topic
+
+    case GameSupervisor.start_game(player) do
+      {:ok, _pid} -> {:reply, :ok, socket}
+      {:error, reason} -> {:reply, {:error, %{reason: inspect(reason)}}, socket}
+    end
   end
 end
