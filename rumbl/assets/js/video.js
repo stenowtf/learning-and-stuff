1006c51dda924a612ce2e1b456beee0896f5b284
Stefano Canducci <steno@fastmail.fm>
2019-08-25 15:13:16 +0200

    rumbl stuff

---
+++ rumbl/assets/js/video.js
@@ -0,0 +1,28 @@
+import Player from "./player"
+
+let Video = {
+
+  init(socket, element) {
+    if (!element) { return }
+    let playerId = element.getAttribute("data-player-id")
+    let videoId = element.getAttribute("data-id")
+    socket.connect()
+    Player.init(element.id, playerId, () => {
+      this.onReady(videoId, socket)
+    })
+  },
+
+  onReady(videoId, socket) {
+    let msgContainer = document.getElementById("msg-container")
+    let msgInput = document.getElementById("msg-input")
+    let postButton = document.getElementById("msg-submit")
+    let vidChannel = socket.channel("videos:" + videoId)
+
+    vidChannel.on("ping", ({ count }) => console.log("PING", count))
+
+    vidChannel.join()
+      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("error", reason => console.log("join failed", reason))
+  }
+}
+export default Video
--- rumbl/lib/rumbl_web/channels/user_socket.ex
+++ rumbl/lib/rumbl_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule RumblWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", RumblWeb.RoomChannel
+  channel "videos:*", RumblWeb.VideoChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
